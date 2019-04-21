8ebae968dbdafe4c3e5dd66175cee9cb41337bce
Lee Changseok <leechangseok@Lees-MacBook-Pro.local>
2019-04-21 00:19:53 -0400

    user last login time add

--- admin/admin_deleteproduct.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('scripts/config.php');
-confirm_logged_in();
-
-if (isset($_GET['delete'])) {
-  $delete_product_id =  $_GET['delete'];
-  deleteItem($delete_product_id);
-}
--- admin/scripts/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'read.php';
-require_once 'login.php';
-require_once 'functions.php';
-require_once 'session.php';
-require_once 'user.php';
-require_once 'product.php';
